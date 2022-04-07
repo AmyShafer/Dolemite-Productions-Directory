0b691d50add67a681b39d44e15a13d085c57b22a
Amy Shafer <amy.u.shafer@gmail.com>
2022-04-07 17:58:07 -0400

    tables started

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS department_db;
+CREATE DATABASE department_db;
+
+USE department_db;
+
+CREATE TABLE departments (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  department_name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE roles (
+  id INT NOT NULL AUTO_INCREMENT,
+  departments_id INT,
+  role_title VARCHAR(100) NOT NULL,
+  role_salary INT NOT NULL,
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL 
+);
+
+CREATE TABLE employees (
+  id INT NOT NULL AUTO_INCREMENT,
+  employee_id INT,
+  employee first_name VARCHAR(30),
+  employee last_name VARCHAR(30),
+  role_id INT NOT NULL,
+  manager_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE SET NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO department (id, department_name) 
+VALUES ("Crew"),
+       ("Cast"),
+       ("Martial Arts"),
+       ("Investors");
+
+INSERT INTO roles (id, title, salary, department_id)
+VALUES ("Director", 5000),
+       ("Writer", 2500),
+       ("Actor", 500),
+       ("Producer", 3000),
+       ("Crew Member", 750),
+       ("Composer/Musician", 1000),
+       ("Martial Arts Fighter", 600);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES ("Rudy Ray", "Moore", ),
+       (),
+       (),
+       (),
+       (),
+       ();
