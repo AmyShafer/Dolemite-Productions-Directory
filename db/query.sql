e948442ea08d5dc0e1888277838ebda78e6eb152
Amy Shafer <amy.u.shafer@gmail.com>
2022-04-08 22:41:08 -0400

    reorganized departments, roles, and employees

--- db/query.sql
+++ db/query.sql
@@ -2,3 +2,4 @@ SELECT departments.department_name AS department, roles.role
 FROM roles LEFT JOIN employees
 ON roles.department_id = departments_id
 ORDER BY employee.last_name;
+
--- index.js
+++ index.js
@@ -1,346 +1,476 @@
-const express = require('express');
 // Import and require mysql2
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+// departments
+let departments = [
+  {
+    key: '1',
+    value: 'Crew'
+  },
+  {
+    key: '2',
+    value: 'Cast'
+  },
+  {
+    key: '3',
+    value: 'Martial Arts'
+  },
+  {
+    key: '4',
+    value: 'Investors'
+  },
+];
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+// roles
+let roles = [
+  {
+    key: '1',
+    value: 'Actor'
+  },
+  {
+    key: '2',
+    value: 'Assistant Cameraman'
+  },
+  {
+    key: '3',
+    value: 'Assistant Director'
+  },
+  {
+    key: '4',
+    value: 'Backer'
+  },
+  {
+    key: '5',
+    value: 'Cinematographer'
+  },
+  {
+    key: '6',
+    value: 'Director'
+  },
+  {
+    key: '7',
+    value: 'Editor'
+  },
+  {
+    key: '8',
+    value: 'Fight Choreographer'
+  },
+  {
+    key: '9',
+    value: 'Gaffer'
+  },
+  {
+    key: '10',
+    value: 'Grip'
+  },
+  {
+    key: '11',
+    value: 'Lightweight Karate Champion'
+  },
+  {
+    key: '12',
+    value: 'Make-up'
+  },
+  {
+    key: '13',
+    value: 'Martial Arts Assistant Director'
+  },
+  {
+    key: '14',
+    value: 'Master Samurai Swordsman'
+  },
+  {
+    key: '15',
+    value: 'Music Arranger'
+  },
+  {
+    key: '16',
+    value: 'Music Consultant'
+  },
+  {
+    key: '17',
+    value: 'Negative Editor'
+  },
+  {
+    key: '18',
+    value: 'Producer'
+  },
+  {
+    key: '19',
+    value: 'Production Assistant'
+  },
+  {
+    key: '20',
+    name: 'Production Manager'
+  },
+  {
+    value: '21',
+    name: 'Set Decorator'
+  },
+  {
+    key: '22',
+    value: 'Sound Editor'
+  },
+  {
+    key: '23',
+    value: 'Sound Engineer'
+  },
+  {
+    key: '24',
+    value: 'Special Effects'
+  },
+  {
+    key: '25',
+    value: 'Writer',
+  },
+];
 
-const sess = {
-  secret: process.env.secret,
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
-};
+// employees
+let employees = [
+  {
+    key: '1',
+    value: 'Malaika Ahraam'
+  },
+  {
+    key: '2',
+    value: 'Al Banks'
+  },
+  {
+    key: '3',
+    value: 'Vince Barbi'
+  },
+  {
+    key: '4',
+    value: 'Joseph Bihari'
+  },
+  {
+    key: '5',
+    value: 'Julius Bihari'
+  },
+  {
+    key: '6',
+    value: 'Lester Bihari'
+  },
+  {
+    key: '7',
+    value: 'Ted Brinson'
+  },
+  {
+    key: '8',
+    value: 'Marie Carter'
+  },
+  {
+    key: '9',
+    value: 'Junior Cranberry'
+  },
+  {
+    key: '10',
+    value: 'Norval Crutcher'
+  },
+  {
+    key: '11',
+    value: 'Gene Davis'
+  },
+  {
+    key: '12',
+    value: 'Von Dutch'
+  },
+  {
+    key: '13',
+    value: 'Warde Egan'
+  },
+  {
+    key: '14',
+    value: 'Mother Fear'
+  },
+  {
+    key: '15',
+    value: 'Bobbie George'
+  },
+  {
+    key: '16',
+    value: 'James Gough'
+  },
+  {
+    key: '17',
+    value: 'Timothy Grace'
+  },
+  {
+    key: '18',
+    value: 'John Ashley Hamilton'
+  },
+  {
+    key: '19',
+    value: 'Josephine Harris'
+  },
+  {
+    key: '20',
+    value: 'Michael Hereford'
+  },
+  {
+    key: '21',
+    value: 'Howard Jackson'
+  },
+  {
+    key: '22',
+    value: 'Claudia Jackson'
+  },
+  {
+    key: '23',
+    value: 'Jerry Jones'
+  },
+  {
+    key: '24',
+    value: 'John Kirkland'
+  },
+  {
+    key: '25',
+    value: 'Jack LaMantiain'
+  },
+  {
+    key: '26',
+    value: 'Jean Lewis'
+  },
+  {
+    key: '27',
+    value: 'Princess Lilio'
+  },
+  {
+    key: '28',
+    value: 'Rex Lipton'
+  },
+  {
+    key: '29',
+    value: 'D’Urville Martin'
+  },
+  {
+    key: '30',
+    value: 'Hugh McDonald'
+  },
+  {
+    key: '31',
+    value: 'Carmella Di Milo'
+  },
+  {
+    key: '32',
+    value: 'Rudy Ray Moore'
+  },
+  {
+    key: '33',
+    value: 'Eric Nero'
+  },
+  {
+    key: '34',
+    value: 'Randy Ornelas'
+  },
+  {
+    key: '35',
+    value: 'Curtis Pulliam'
+  },
+  {
+    key: '36',
+    value: 'Hy Pyke'
+  },
+  {
+    key: '37',
+    value: 'Lady Reed'
+  },
+  {
+    key: '38',
+    value: 'Marvin Reesee'
+  },
+  {
+    key: '39',
+    value: 'George Selin'
+  },
+  {
+    key: '40',
+    value: 'Nicholas Josef von Sternberg'
+  },
+  {
+    key: '41',
+    value: 'Bruce Stevens'
+  },
+  {
+    key: '42',
+    value: 'John Wagner'
+  },
+  {
+    key: '43',
+    value: 'Harry Wollman'
+  },
+  {
+    key: '44',
+    value: 'Arthur Wright'
+  },
+  {
+    key: '45',
+    value: 'Carol Yasunaga'
+  }
+];
 
-app.use(session(sess));
-
+// I need a Main Menu
 async function initDirectory() {
-  const answers = await inquirer.prompt([
+  const questions = await inquirer.prompt([
     {
       type: 'list',
       message: 'What would you like to do, Baby? (Use arrow keys)',
       name: 'mainMenu',
-      choice: [
+      choices: [
+        // employee questions
         {  
-          name: 'View All Employees',
+          key: 'a',
+          value: 'View All Employees',
+        },
+        {  
+          key: 'b',
+          value: 'Add Employee',
+        },
+        {  
+          key: 'c',
+          value: 'Remove Employee',
         },
         {
-          name: 'View All Employees By Department',
+          key: 'd',
+          value: 'Add Employee',
         },
         {
-          name: 'View All Employees By Manager',
+          key: 'e',
+          value: 'Remove Employee',
         },
         {
-          name: 'Add Employee',
+          key: 'f',
+          value: 'Update Employee Role',
         },
         {
-          name: 'Remove Employee',
-        },
-        {
-          name: 'Update Employee Role',
-        },
-        {
+          key: 'g',
           name: 'Update Employee Manager',
         },
+        // role questions
         {
-          name: 'View All Roles',
+          key: 'h',
+          value: 'View All Roles',
         },
         {
-          name: 'Add Role',
+          key: 'i',
+          value: 'Add Role',
         },
         {
-          name: 'Remove Role',
+          key: 'j',
+          value: 'Remove Role',
+        },
+        // department questions
+        {
+          key: 'k',
+          value: 'View All Departments',
         },
         {
-          name: 'View All Departments',
+          key: 'l',
+          value: 'Add Department',
         },
         {
-          name: 'Add Department',
+          key: 'm',
+          value: 'Remove Departments',
         },
         {
-          name: 'Remove Departments',
+          key: 'n',
+          value: 'View Total Budget By Department',
         },
         {
-          name: 'View Total Budget By Department',
-        },
-        {
-          name: 'Quit',
+          key: 'o',
+          value: 'Quit',
         },
       ],
     },
-  ]),
+  ]).then(answers => {
+    console.log(answers);
+  })
 }
 
+// I need there to be department related tasks
 async function departmentQuestions() {
-  const answers = await inquirer.prompt([
+  const questions = await inquirer.prompt([
     {
       type: 'list',
-      message: 'What would you like to do, Baby?',
+      message: 'What do you wanna, Baby?',
       name: 'departmentOptions',
       choices: [
         {
-          name: 'Add A Department',
+          key: 'a',
+          value: 'Add A Department',
         },
         {
-          name: 'Remove A Department',
+          key: 'b',
+          value: 'Remove A Department',
         },
         {
-          name: 'View A Department\'s Budget',
+          key: 'c',
+          value: 'View Budget Of A Department',
         },
       ],
-         // Add a Department
-         {
-           type: 'input',
-           message: 'What is the name of the department you would like to add?',
-           name: 'newDepartment',
-         },
- 
-         // Remove a Department
-          name: 'What is the name of the department you would like to remove?',
-          choices: [
-            {
-              name: 'Crew',
-            },
-            {
-              name: 'Cast',
-            },
-            {
-              name: 'Martial Arts',
-            },
-            {
-              name: 'Investors',
-            },
-          ],
-        },
-        {
-          // Department Budget
-          name: 'What department budget would you like to see?',
-          choices: [
-            {
-              name: 'Crew',
-            },
-            {
-              name: 'Cast',
-            },
-            {
-              name: 'Martial Arts',
-            },
-            {
-              name: 'Investors',
-            },
-          ],
-        }
-   ])
+    },
+     // Add a Department
+    {
+        type: 'input',
+        message: 'What\'s the name of the department you wanna add?',
+        name: 'newDepartment',
+    },
+    // Remove a Department
+    {
+        name: 'What\'s  the name of the department you wanna remove?',
+        choices: departments,
+      },
+      // Department Budget
+      {
+        name: 'What department budget do you wanna see?',
+        choices: departments,
+      },
+   ]).then(answers => {
+    console.log(answers);
+  })
 }
 
-
+// I need there to be employee related tasks
 async function employeeQuestions() {
-  const answers = await inquirer.prompt([
+  const questions = await inquirer.prompt([
     {
       type: 'input',
       message: 'Please give details about the new employee:',
       name: 'employeeOptions',
-
-        {
-          type: 'input',
-          name: 'What is the employee\'s first name?',
-        },
-        {
-          type: 'input',
-          name: 'What is the employee\'s last name?',
-        },
-        {
-          type: 'list',
-          name: 'What is the employee\'s role?',
-          choices: [
-            {
-              name: 'Actor',
-            },
-            {
-              name: 'Assistant Cameraman',
-            },
-            {
-              name: 'Assistant Director',
-            },
-            {
-              name: 'Backer',
-            },
-            {
-              name: 'Cinematographer',
-            },
-            {
-              name: 'Director',
-            },
-            {
-              name: 'Editor',
-            },
-            {
-              name: 'Fight Choreographer',
-            },
-            {
-              name: 'Gaffer',
-            },
-            {
-              name: 'Grip',
-            },
-            {
-              name: 'Lightweight Karate Champion',
-            },
-            {
-              name: 'Make-up',
-            },
-            {
-              name: 'Martial Arts Assistant Director',
-            },
-            {
-              name: 'Master Samurai Swordsman',
-            },
-            {
-              name: 'Music Arranger',
-            },
-            {
-              name: 'Music Consultant',
-            },
-            {
-              name: 'Negative Editor',
-            },
-            {
-              name: 'Producer',
-            },
-            {
-              name: 'Production Assistant',
-            },
-            {
-              name: 'Production Manager',
-            },
-            {
-              name: 'Set Decorator',
-            },
-            {
-              name: 'Sound Editor',
-            },
-            {
-              name: 'Sound Engineer',
-            },
-            {
-              name: 'Special Effects',
-            },
-            {
-              name: 'Writer',
-            }
-          ],
-        {
-          type: 'list',
-          name: 'Who is the employee\'s manager?',
-          choices: [
-            {
-              name: 'Malaika Ahraam',
-            },
-            {
-              name: 'Al Banks',
-            },
-            {
-              name: 'Vince Barbi',
-            },
-          ]
-        },
-      }
     },
-  ])
+    {
+      type: 'input',
+      name: 'What is the employee\'s first name?',
+    },
+    {
+      type: 'input',
+      name: 'What is the employee\'s last name?',
+    },
+    {
+      type: 'list',
+      name: 'What is the employee\'s role?',
+      choices: roles,
+    },
+    {
+      type: 'list',
+      name: 'Who is the employee\'s manager?',
+      choices: employees,
+    }
+  ]).then(answers => {
+    console.log(answers);
+  })
 }
 
+// I need there to be role related tasks
 async function roleQuestions() {
-  const answers = await inquirer.prompt([
+  const questions = await inquirer.prompt([
     {
       type: 'list',
       message: 'View All Roles:',
       name: 'listOfRoles',
-      choices: [
-        {
-          name: 'Actor',
-        },
-        {
-          name: 'Assistant Cameraman',
-        },
-        {
-          name: 'Assistant Director',
-        },
-        {
-          name: 'Backer',
-        },
-        {
-          name: 'Cinematographer',
-        },
-        {
-          name: 'Director',
-        },
-        {
-          name: 'Editor',
-        },
-        {
-          name: 'Fight Choreographer',
-        },
-        {
-          name: 'Gaffer',
-        },
-        {
-          name: 'Grip',
-        },
-        {
-          name: 'Lightweight Karate Champion',
-        },
-        {
-          name: 'Make-up',
-        },
-        {
-          name: 'Martial Arts Assistant Director',
-        },
-        {
-          name: 'Master Samurai Swordsman',
-        },
-        {
-          name: 'Music Arranger',
-        },
-        {
-          name: 'Music Consultant',
-        },
-        {
-          name: 'Negative Editor',
-        },
-        {
-          name: 'Producer',
-        },
-        {
-          name: 'Production Assistant',
-        },
-        {
-          name: 'Production Manager',
-        },
-        {
-          name: 'Set Decorator',
-        },
-        {
-          name: 'Sound Editor',
-        },
-        {
-          name: 'Sound Engineer',
-        },
-        {
-          name: 'Special Effects',
-        },
-        {
-          name: 'Writer',
-        }
-      ],
+      choices: roles,
     },
     {
       type: 'input',
@@ -350,93 +480,23 @@ async function roleQuestions() {
     {
       type: 'list',
       message: 'What role would you like to remove?',
-      choices: [
-        {
-          name: 'Actor',
-        },
-        {
-          name: 'Assistant Cameraman',
-        },
-        {
-          name: 'Assistant Director',
-        },
-        {
-          name: 'Backer',
-        },
-        {
-          name: 'Cinematographer',
-        },
-        {
-          name: 'Director',
-        },
-        {
-          name: 'Editor',
-        },
-        {
-          name: 'Fight Choreographer',
-        },
-        {
-          name: 'Gaffer',
-        },
-        {
-          name: 'Grip',
-        },
-        {
-          name: 'Lightweight Karate Champion',
-        },
-        {
-          name: 'Make-up',
-        },
-        {
-          name: 'Martial Arts Assistant Director',
-        },
-        {
-          name: 'Master Samurai Swordsman',
-        },
-        {
-          name: 'Music Arranger',
-        },
-        {
-          name: 'Music Consultant',
-        },
-        {
-          name: 'Negative Editor',
-        },
-        {
-          name: 'Producer',
-        },
-        {
-          name: 'Production Assistant',
-        },
-        {
-          name: 'Production Manager',
-        },
-        {
-          name: 'Set Decorator',
-        },
-        {
-          name: 'Sound Editor',
-        },
-        {
-          name: 'Sound Engineer',
-        },
-        {
-          name: 'Special Effects',
-        },
-        {
-          name: 'Writer',
-        }
-      ],
+      choices: roles,
     }
-  ])
+  ]).then(answers => {
+    console.log(answers);
+  })
 }
 
-// start server and connect to db
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now Listening'));
-}).catch(function(err){
-  console.log(err)
-})
+// Pull data from the user inputs to dynamically create the cms
+const generateDirectory = async() => {
+  console.log("Hold on your seats!");
+  initDirectory();
+
+}
+
+generateDirectory();
+
+
 
 
 
