e2ccb0928c060e14016f0d155e1b84493ee3e57c
Amy Shafer <amy.u.shafer@gmail.com>
2022-04-12 10:12:47 -0400

    working on the query file

--- db/query.sql
+++ db/query.sql
@@ -1,4 +1,4 @@
-SELECT employees AS department, roles.role
+SELECT employees AS allEmployeeInfo, roles.role_title,  department.department_name, roles.role_salary, employees.manager_id
 FROM roles LEFT JOIN employees
 ON roles.department_id = departments_id
 ORDER BY employee.last_name;
